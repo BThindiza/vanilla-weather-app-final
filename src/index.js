dec4d6bf77a8ecdfc9e1174eed0cf0df8c6a4092
Boitumelo Thindiza <tthindiza@gmail.com>
2023-11-20 12:34:42 -0800

    made adjustments to my app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function refreshWeather(){
     let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
+  let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
